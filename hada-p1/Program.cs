29986ba85ed3ad3326756dd89bd5e62fd041a3dc
Esteban Antón Sellés <eas45@alu.ua.es>
2019-02-11 14:51:42 +0100

    Main acabado

--- hada-p1/Program.cs
+++ hada-p1/Program.cs
@@ -10,6 +10,40 @@ namespace hada_p1
     {
         static void Main(string[] args)
         {
+            string opt;
+            string data;
+            double conv;
+
+            do
+            {
+
+                Console.WriteLine("--- MENÚ DE CONVERSIÓN ---");
+                Console.WriteLine(" 1 - Segundos -> Minutos");
+                Console.WriteLine(" 2 - Minutos -> Segundos");
+                Console.WriteLine(" 3 - Salir");
+                Console.WriteLine("Inserta el número de la opción: ");
+                opt = Console.ReadLine();
+
+                switch (opt)
+                {
+                    case "1":
+                        Console.WriteLine("\nInserta los segundos que quieres convertir en minutos: ");
+                        data = Console.ReadLine();
+                        Console.WriteLine("\n{0}", HadaP1.Seconds2Minutes(Double.Parse(data)));
+                        break;
+                    case "2":
+                        Console.WriteLine("\nInserta los minutos que quieres convertir en segundos: ");
+                        data = Console.ReadLine();
+                        Console.WriteLine("\n{0}", HadaP1.Minutes2Seconds(Double.Parse(data)));
+                        break;
+                    case "3":
+                        Console.WriteLine("Hasta luego");
+                        break;
+                    default:
+                        Console.WriteLine("ERROR! - Opción desconocida\n");
+                        break;
+                }
+            } while (opt != "3");
         }
     }
 }
